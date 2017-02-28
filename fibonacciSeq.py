bc7d0d68e65ce7eb4e3447780e4d570c34d931d1
gabrielamelian <gabrielamelian@gmail.com>
2017-02-28 15:32:15 +1300

    Adds function to get Fibonacci number

---
+++ fibonacciSeq.py
@@ -0,0 +1,11 @@
+"""
+Solution to Fibonacci problem for a single number.
+Takes a long time to run.
+"""
+
+def F(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return F(n-1)+F(n-2)
+
+print F(34)
