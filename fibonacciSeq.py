8b2e21a439bcfff5ea7f30329644bd5eb263953f
gabrielamelian <gabrielamelian@gmail.com>
2017-02-28 15:54:32 +1300

    Adds function to print fibonacci sequence up to 100 times

--- fibonacciSeq.py
+++ fibonacciSeq.py
@@ -3,9 +3,26 @@ Solution to Fibonacci problem for a single number.
 Takes a long time to run.
 """
 
-def F(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return F(n-1)+F(n-2)
+# def F(n):
+#     if n == 0: return 0
+#     elif n == 1: return 1
+#     else: return F(n-1)+F(n-2)
+#
+# print F(34)
 
-print F(34)
+"""
+Solution to Fibonacci sequence.
+Made to run up to 100 times
+"""
+
+def Fseq():
+    i = 0
+    a,b = 0,1
+    print a
+    print b
+    while i < 100:
+        i += 1
+        a, b = b, a + b
+        print b
+
+Fseq()
