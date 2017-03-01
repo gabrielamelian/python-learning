8d12d84c0e7860675513a78e23dd21ac7216c1c8
gabrielamelian <gabrielamelian@gmail.com>
2017-03-02 11:32:59 +1300

    Adds solution passes all tests

--- classy.py
+++ classy.py
@@ -24,6 +24,20 @@ class Classy(object):
     def __init__(self):
         self.items = []
 
+    def addItem(self, itemName):
+        self.items.append(itemName)
+
+    def getClassiness(self):
+        classiness = 0
+        for item in self.items:
+            if item == "tophat":
+                classiness += 2
+            elif item == "bowtie":
+                classiness += 4
+            elif item == "monocle":
+                classiness += 5
+        return classiness
+
 
 # Test cases
 me = Classy()
