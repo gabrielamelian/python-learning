ffcfb9935ee91548a296a1636075ef0ed82c9b98
gabrielamelian <gabrielamelian@gmail.com>
2017-03-01 20:04:19 +1300

    Adds solution to second challenge with while loop

--- pythonic_algorithms.py
+++ pythonic_algorithms.py
@@ -1,10 +1,30 @@
 """Code snippets from CodeAcademy Pythonic Algorithms course."""
 
-i = 100
+# i = 100
+#
+# while True:
+#     i -= 1
+#     if i == 0:
+#         break
+#
+# print i
 
+"""
+First, run this code, noticing its slow running speed.
+Then, change the loop so that it utilizes an infinite
+loop to accomplish the same task.
+
+
+j = 0
+for i in range(1, 1000001):
+    j += 1
+    print j
+
+"""
+
+j = 0
 while True:
-    i -= 1
-    if i == 0:
+    j += 1
+    if j == 1000000:
         break
-
-print i
+print j
