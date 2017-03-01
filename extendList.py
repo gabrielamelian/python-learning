c4d0f1fdeb7df13f90d36a7d6a965f18f1f46689
gabrielamelian <gabrielamelian@gmail.com>
2017-03-01 17:33:18 +1300

    Fixes indentation and other linting

--- extendList.py
+++ extendList.py
@@ -1,6 +1,6 @@
 
 """
-Solution to extendList problem
+Solution to extendList problem.
 This issue comes from the way the values are
 assigned in memory.
 
@@ -11,10 +11,12 @@ def extendList(val, list=[]):
 
 """
 
+
 def extendList(val):
     list = []
     list.append(val)
     return list
+
 
 list1 = extendList(10)
 list2 = extendList(123)
