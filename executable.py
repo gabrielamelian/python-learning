0370323c37dcf75687e8a5f43337c80451505618
gabrielamelian <gabrielamelian@gmail.com>
2017-03-26 21:35:49 +1300

    Adds argparse and says hello with the script arguments

--- executable.py
+++ executable.py
@@ -4,4 +4,11 @@
 Test program executable file
 """
 
-print "Hello World"
+import argparse
+
+parser = argparse.ArgumentParser(description='Requires a name')
+
+parser.add_argument('-n', '--name')
+arguments = parser.parse_args()
+
+print "Hello %s" % arguments.name
